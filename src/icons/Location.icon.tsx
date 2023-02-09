2739c755fcfb9a924094b2103178d0e0080e59bb
Sahil Kanjariya <sahil.kanjariya@smartsensesolutions.com>
2023-02-09 15:21:17 +0530

    Remove the unused imports

--- src/icons/Location.icon.tsx
+++ src/icons/Location.icon.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { IconType } from '../types/common';
-import { Theme } from '@foster/theme/default';
 import styled from 'styled-components';
 type ColorType = {
     fill: string;
